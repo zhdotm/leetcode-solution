6797fccd655053f34b54154df9dbc1fbe5d5dc55
毛志浩 <85991202+zhdotm@users.noreply.github.com>
2022-08-11 17:31:42 +0800

    feat: 添加题目

---
+++ src/main/java/leetcode/solution/AddTwoNumbers.java
@@ -0,0 +1,60 @@
+package leetcode.solution;
+
+/**
+ * 给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+ * <p>
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0开头。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhihao.mao
+ */
+public class AddTwoNumbers {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultListNode = new ListNode();
+        ListNode currentListNode = resultListNode;
+        boolean isNeedPlusOne = Boolean.FALSE;
+        while (Boolean.TRUE) {
+            int resultVal = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + (isNeedPlusOne ? 1 : 0);
+            isNeedPlusOne = resultVal / 10 > 0;
+            currentListNode.val = resultVal % 10;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+            if (l1 == null && l2 == null) {
+                if (isNeedPlusOne) {
+                    ListNode nextListNode = new ListNode();
+                    nextListNode.val = 1;
+                    currentListNode.next = nextListNode;
+                }
+                break;
+            }
+            ListNode nextListNode = new ListNode();
+            currentListNode.next = nextListNode;
+            currentListNode = nextListNode;
+        }
+
+        return resultListNode;
+    }
+}
---
+++ src/main/java/leetcode/solution/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,28 @@
+package leetcode.solution;
+
+import java.util.HashMap;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ * @author zhihao.mao
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
